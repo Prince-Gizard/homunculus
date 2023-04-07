5d92fcb56f76ce3740b2549d727acdaef1aedde0
Василий Бураев <buraevvasilii@gmail.com>
2023-04-07 15:35:27 +0300

    add serch form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Header } from "./components/Header";
+import { Controls } from "./components/Controls";
+import { Main } from "./components/Main";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main>
+        <Controls />
+      </Main>
     </div>
   );
 }
